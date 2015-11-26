3ab7c1f4cee7d5b3ea2cff988a403e5de350ca41
Lyle Dean <lyle.dean1@gmail.com>
2015-11-26 03:34:50 +0000

    working checkpoint

--- static/javascripts/posts/directives/post.directive.js
+++ static/javascripts/posts/directives/post.directive.js
@@ -1,19 +1,31 @@
-(function(){
-    'use strict';
-    
-    angular
-        .module('thinkster.posts.directives')
-        .directive('post', post);
-        
-    function post(){
-        var directive = {
-            restrict: 'E',
-            scope: {
-                post: '='
-            },
-            templateUrl = '/static/templates/posts/post.html'
-        };
-        
-        return directive;
-    }
+/**
+* Post
+* @namespace thinkster.posts.directives
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.posts.directives')
+    .directive('post', post);
+
+  /**
+  * @namespace Post
+  */
+  function post() {
+    /**
+    * @name directive
+    * @desc The directive to be returned
+    * @memberOf thinkster.posts.directives.Post
+    */
+    var directive = {
+      restrict: 'E',
+      scope: {
+        post: '='
+      },
+      templateUrl: '/static/templates/posts/post.html'
+    };
+
+    return directive;
+  }
 })();
--- static/javascripts/posts/directives/posts.directive.js
+++ static/javascripts/posts/directives/posts.directive.js
@@ -1,21 +1,33 @@
-(function(){
-    'use strict';
-    
-    angular
-        .module('thinkster.posts.directives')
-        .directive('posts', posts);
-        
-    function posts(){
-        var directive = {
-            controlller: 'PostsController',
-            controllerAs: 'vm',
-            restrict: 'E',
-            scope: {
-                posts: '='
-            },
-            templateUrl: '/static/templates/posts/posts.html'
-        };
-        
-        return directive;
-    }
+/**
+* Posts
+* @namespace thinkster.posts.directives
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.posts.directives')
+    .directive('posts', posts);
+
+  /**
+  * @namespace Posts
+  */
+  function posts() {
+    /**
+    * @name directive
+    * @desc The directive to be returned
+    * @memberOf thinkster.posts.directives.Posts
+    */
+    var directive = {
+      controller: 'PostsController',
+      controllerAs: 'vm',
+      restrict: 'E',
+      scope: {
+        posts: '='
+      },
+      templateUrl: '/static/templates/posts/posts.html'
+    };
+
+    return directive;
+  }
 })();
